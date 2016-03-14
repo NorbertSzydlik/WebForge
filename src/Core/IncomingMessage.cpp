c0a7ce197509da27d2ba8c4467861cbdccad22be
Norbert Szydlik <norbert.szydlik@gmail.com>
2016-03-14 21:57:03 +0100

    Add simple cookie support

--- src/Core/IncomingMessage.cpp
+++ src/Core/IncomingMessage.cpp
@@ -1,20 +1,45 @@
 #include "IncomingMessage.hpp"
 #include <regex>
 #include <iostream>
+#include <boost/tokenizer.hpp>
 
 using namespace WebForge::http;
 
 std::regex headerKeyValueRegex{"^([a-zA-Z-]+):\\s+(\\S+)"};
+using CharTokenizer = boost::tokenizer<boost::char_separator<char> >;
+boost::char_separator<char> cookieSeparator(";");
 
 IncomingMessage::IncomingMessage(std::vector<std::string> &&linesUploaded) : m_linesUploaded(linesUploaded)
 {
-    for(auto& line : m_linesUploaded) {
+    for(auto& line : m_linesUploaded)
+    {
         std::smatch headerKeyValueMatch;
-        std::regex_match (line, headerKeyValueMatch, headerKeyValueRegex);
+        std::regex_match(line, headerKeyValueMatch, headerKeyValueRegex);
 
-        if(headerKeyValueMatch.size() == 3) {
-            HeaderName headerName(headerKeyValueMatch.str(1));
-            HeaderValue headerValue(headerKeyValueMatch.str(2));
+        if (headerKeyValueMatch.size() != 3)
+        {
+            continue;
+        }
+        HeaderName headerName(headerKeyValueMatch.str(1));
+        HeaderValue headerValue(headerKeyValueMatch.str(2));
+        if(headerName == "Cookie")
+        {
+            CharTokenizer cookiePairs(headerValue, cookieSeparator);
+            for(std::string const& cookiePair : cookiePairs)
+            {
+                auto assignmentPosition = cookiePair.find("=");
+                if (assignmentPosition == std::string::npos)
+                {
+                    continue;
+                }
+                auto cookieName = cookiePair.substr(0, assignmentPosition);
+                auto cookieValue = cookiePair.substr(assignmentPosition + 1);
+                std::cout << "COOKIE: " << cookieName << "=" << cookieValue << std::endl;
+                m_cookies[cookieName] = cookieValue;
+            }
+        }
+        else
+        {
             m_headers[headerName] = headerValue;
         }
     }
